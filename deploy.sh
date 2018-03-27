d9fe5b5535c0e33a5088fcbaa82f3981cbfb0492
Emanuele Rocca <ema@wikimedia.org>
2018-03-27 06:00:03 +0200

    Further ugliness removed from deploy script
    Crazy how much simpler life can be.

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 
-BASEDIR="_site/"
-DESTDIR="."
-
-directories=`find $BASEDIR -type d | sed s#$BASEDIR##g`
-files=`find $BASEDIR -type f -name \*.html -or -name \*.xml| sed s#$BASEDIR##g`
-
-do_stuff() {
-  #echo "$1"
-  echo "$1" | cadaver http://www2.linux.it/davhome/ema
-}
-
-jekyll build
-
-do_stuff "mkdir $DESTDIR"
-
-for dir in $directories
-do
-  do_stuff "mkdir $DESTDIR/$dir"
-done
-
-for file in $files
-do
-  do_stuff "put $BASEDIR/$file $DESTDIR/$file"
+find . -name \*.html -or -name \*.pdf | while read file; do
+  echo "put $file $file" | cadaver http://www2.linux.it/davhome/ema
 done
